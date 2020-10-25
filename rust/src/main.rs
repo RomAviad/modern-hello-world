9a93d157efd070519aa784341a1986218ad76fd9
Aviad Rom <aviad@zebra-med.com>
2020-10-25 14:09:20 +0200

    cleaned warnings and return json format from ngrams call

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-#[macro_use]extern crate serde;
+extern crate serde;
 extern crate rocket_contrib;
 extern crate ngrams;
 
+use ngrams::Ngram;
+use rocket::response::content;
 use rocket_contrib::json::Json;
 use serde::{Deserialize};
-use ngrams::Ngram;
 
 const TEXT_LIMIT: u64 = 1024;
 
@@ -21,18 +22,16 @@ struct NgramRequest<'a> {
 
 #[get("/")]
 fn hello() -> String {
-    let my_result = "Hello World";
-//    let tokens: Vec<String> = my_result.split(" ").map(|s| s.to_string()).collect();
-    format!("{}", my_result)
+    format!("Hello World")
 }
 
 #[post("/ngram", format = "application/json", data = "<body>")]
-fn ngrams(body: Json<NgramRequest>) -> String {
+fn ngrams(body: Json<NgramRequest>) -> content::Json<String> {
     let tokens = body.data.split(" ");
     let n: usize = body.n;
     let result: Vec<_> = tokens.ngrams(n).collect();
-
-    format!("{:?}", result)
+    let result_str = format!("{:?}", result);
+    content::Json(result_str)
 }
 
 fn main() {
